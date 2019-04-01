3f60952bf76430ab9f9c191fce93fb113d5dd0b3
Christopher Viel <viel.christopher@gmail.com>
2019-03-31 21:36:35 -0400

    feat: add install and setup scripts

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 This repository contains bootstrapping scripts to get the media server up and running.
 
+## Arch Linux install
+
+1. Boot live environment
+1. Setup network
+1. Clone this repository on the live environment
+1. Run `install.sh`
+1. Once rebooted, run `setup.sh`
+
+## Partition summary
+
+Mount | Size | FS | Parity | Comments
+--- | --- | --- | --- | ---
+\[SWAP] | | | | Nope
+/boot | 550&nbsp;MB | ? | N | 
+/ | 32&nbsp;GB | Ext4 | N | (system) appdata
+/data | 6&nbsp;TB | BTRFS | N | (high write)<br>tmp, torrents, downloads, extraction, minecraft
+/storage | 18&nbsp;TB | XFS| Y | (high read, low write)<br>movies, series, music, ebooks, custom backups.<br>**Max 6 disks** pooled by **MergerFS**.
+/storageparity | \<*> | XFS | - | Managed by **Snapraid**.<br>Disk size is the **biggest disk from /storage**
+
+## Resources
+
+https://wiki.archlinux.org/index.php/installation_guide
---
+++ setup.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+echo "Installing binaries..."
+pacman -Syu btrfs-progs sudo ssh nftables ethtool vim fish docker docker-compose
+
+git clone https://aur.archlinux.org/yay.git
+( cd yay ; makepkg -si )
+rm -rf yay
+
+
+echo "Configuring permissions..."
+groupadd poweroff
+groupadd ssh
+
+echo '%poweroff ALL=NOPASSWD:/usr/bin/poweroff' > /etc/sudoers.d/poweroff
+echo '%wheel ALL=(ALL) ALL' > /etc/sudoers.d/wheel
+chmod 0440 /etc/sudoers.d/poweroff /etc/sudoers.d/wheel
+
+
+echo "Configuring WOL..."
+ethtool -s enp2s0 wol g
+cat ./configs/50-wired.link > /etc/systemd/network/50-wired.link
+
+echo "Configuring ssh daemon..."
+cat ./configs/sshd_config  > /etc/ssh/sshd_config
+
+echo "Configuring NFTables..."
+cat ./configs/nftables.conf > /etc/nftables.conf
+
+
+echo "Setting up 'avatar'..."
+
+useradd -m -G wheel,poweroff,ssh -S /usr/bin/fish avatar
+
+echo "Enter avatar's password: "
+passwd avatar
+
+su avatar -c <<EOF
+curl -L https://get.oh-my.fish | fish;
+omf i sudope archlinux colored-man-pages bobthefish;
+
+mkdir -p /home/avatar/.ssh;
+touch /home/avatar/.ssh/authorized_keys;
+
+chmod u=rwx,g=,o= /home/avatar/.ssh;
+chmod u=rw,g=,o= /home/avatar/.ssh/authorized_keys;
+EOF
+
+cat ./configs/avatar-authorized_keys > /home/avatar/.ssh/authorized_keys
+
+echo 'avatar setup complete!'
+
+
+echo "Setting up 'homeassistant'..."
+
+useradd -m -G poweroff,ssh -S /bin/bash homeassistant
+
+echo "Enter homeassistant's password: "
+passwd homeassistant
+
+su avatar -c <<EOF
+mkdir -p /home/homeassistant/.ssh;
+touch /home/homeassistant/.ssh/authorized_keys;
+
+chmod u=rwx,g=,o= /home/homeassistant/.ssh;
+chmod u=rw,g=,o= /home/homeassistant/.ssh/authorized_keys;
+EOF
+
+cat ./configs/homeassistant-authorized_keys > /home/homeassistant/.ssh/authorized_keys
+
+echo 'homeassistant setup complete! Make sure to `ssh avatar-automate-hass "ssh media | yes"`.'
+
+
+echo "Setting up storage disks..."
+# TODO: /data and /storage
+#  Partition other hard drives
+#  Install and configure mergerfs
+#  Install and configure snapraid
+#  Update fstab
+#  Initialize media folders
+
+
+echo "Starting services..."
+
+systemctl enable sshd nftables docker --now
+
+# TODO: Copy docker-compose .env secrets and finally start containers
