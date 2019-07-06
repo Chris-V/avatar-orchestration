5ef4daab9878e2aa258d7be40178381b8cb4038e
Christopher Viel <viel.christopher@gmail.com>
2019-07-06 12:02:22 -0400

    fix: backup filters should be relative to /appdata

--- ansible/roles/automate/files/appdata/appdata-backup/backup_acl.sh
+++ ansible/roles/automate/files/appdata/appdata-backup/backup_acl.sh
@@ -9,6 +9,7 @@ if ! [ -x "$(command -v /usr/bin/getfacl)" ]; then
   /sbin/apk add --quiet --no-cache acl
 fi
 
+umask 007
 /usr/bin/getfacl -R "$SYNC_SRC" > "$SYNC_SRC"/acl.txt
 
 echo "INFO: ACL written to '$SYNC_SRC/acl.txt'"
